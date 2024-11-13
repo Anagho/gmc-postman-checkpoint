cc7d03d96a52370fc89674585f0e4ac86dc0f45c
Anagho <anaghosamroyce@gmail.com>
2024-11-13 08:07:15 +0100

    new changes

--- models/userModel.js
+++ models/userModel.js
@@ -12,6 +12,17 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+// Pre-save hook to set the status based on age
+userSchema.pre('save', function (next) {
+  // If age is less than 20, set status to 'inactive'
+  if (this.age < 20) {
+    this.status = 'inactive';
+  } else {
+    this.status = 'active';  // Otherwise, set status to 'active'
+  }
+  next(); // Proceed to save the document
+});
+
 // Create the User model
 const User = new mongoose.model("User", userSchema);
 
